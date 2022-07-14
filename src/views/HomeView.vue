11f4108edcb7f622886a02a5ecee4c94d63c926a
Wahid Rizqi Akbar Nurjaman <wahidrizqiakbarn@gmail.com>
2022-07-14 08:42:03 +0700

    home page component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+import { RouterView } from 'vue-router'
+import Navbar from '@/components/Navbar.vue'
+import { MenuIcon } from '@heroicons/vue/solid'
+</script>
+
+<script>
+export default {
+  methods: {
+    toListView() {
+			this.$router.push('/list-content')
+		}
+  }
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <Navbar>
+    <template #icon>
+      <MenuIcon 
+        class="h-6 w-6 text-blueish hover:text-blue-500"
+        @click="toListView()" />
+    </template>
+  </Navbar>
+  <RouterView />
 </template>
