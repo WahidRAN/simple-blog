10c19fd37ac155df5aa49a44ee6614ccab9e4410
Wahid Rizqi Akbar Nurjaman <wahidrizqiakbarn@gmail.com>
2022-07-14 08:42:32 +0700

    Routing for details and list

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import HomeView from '../views/HomeView.vue'
+import ListView from '../views/ListView.vue'
+
+import Content from '../components/Content.vue'
+import Details from '../components/Details.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,16 +12,24 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      children: [
+        {
+          name: "content",
+          path: '',
+          component: Content
+        },
+        {
+          path: 'details',
+          component: Details
+        }
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/list-content',
+      name: 'listContent',
+      component: ListView
+    },
   ]
 })
 
